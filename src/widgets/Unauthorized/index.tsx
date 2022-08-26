210e620ab1518461735b00a5cb3938908a10c524
Marko <drakulic.marko0@gmail.com>
2022-08-26 17:11:53 +0200

    Updated unauthorized widget

--- src/widgets/Unauthorized/index.tsx
+++ src/widgets/Unauthorized/index.tsx
@@ -5,10 +5,10 @@ import { Button } from "../../components";
 import unauthorized from "../../assets/unauthorized.svg";
 const index = () => {
   return (
-    <div className="w-screen h-screen flex flex-col justify-center gap-8">
+    <div className="container mx-auto w-screen h-screen flex flex-col justify-center gap-8 px-4">
       <Image src={unauthorized} alt="Unauthorized" width={400} height={400} />
       <div className="flex flex-col items-center justify-start gap-4">
-        <h1 className="font-bold text-3xl">
+        <h1 className="font-bold text-3xl text-center">
           Nemate pristup ili niste prijavljeni
         </h1>
         <Link href="/" passHref>
