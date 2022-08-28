66dc6f83f2ce89d34dbbaea0fcd4872e2696d4af
Marko <drakulic.marko0@gmail.com>
2022-08-28 12:28:24 +0200

    Ease of life sidebar update

--- src/components/SidebarItem/index.tsx
+++ src/components/SidebarItem/index.tsx
@@ -9,16 +9,16 @@ type SidebarItemProps = {
 const SidebarItem: FC<SidebarItemProps> = ({ label, icon, onClick }) => {
   return (
     <li
-      onClick={onClick}
+      
       className="flex justify-center px-2 py-1.5 text-gray-500 rounded hover:bg-gray-50 hover:text-gray-700 relative group hover:cursor-pointer">
       <>
-        {React.createElement("svg", { className: "w-5 h-5 opacity-75" }, icon)}
+        {React.createElement("svg", { className: "w-5 h-5 opacity-75",onClick:onClick }, icon)}
 
-        <span className="absolute text-xs font-medium text-white bg-gray-900 left-full ml-4 px-2 py-1.5 top-1/2 -translate-y-1/2 rounded opacity-0 group-hover:opacity-100 z-50">
+        <span className="absolute text-xs font-medium text-white bg-gray-900 left-full ml-4 px-2 py-1.5 top-1/2 -translate-y-1/2 rounded opacity-0 group-hover:opacity-100 z-50 pointer-events-none">
           {label}
         </span>
       </>
-    </li>
+    </li> 
   );
 };
 
