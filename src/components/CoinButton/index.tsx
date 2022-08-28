4eae02ff79f1632268d635cebbfd5f13f273568d
Marko <drakulic.marko0@gmail.com>
2022-08-28 16:05:06 +0200

    Updated coin button selected state ui

--- src/components/CoinButton/index.tsx
+++ src/components/CoinButton/index.tsx
@@ -14,8 +14,10 @@ const CoinButton: FC<CoinButtonProps> = ({
   return (
     <button
       onClick={() => onClick(coinsAmount)}
-      className={`flex flex-col items-center justify-center text-2xl p-12  text-white font-semibold rounded-full w-5 h-5 hover:bg-[#3d74a1] ${
-        currentAmount === coinsAmount ? "bg-[#6587a2]" : "bg-[#467BA5]"
+      className={`flex flex-col items-center justify-center text-2xl p-12  text-white font-semibold rounded-full w-5 h-5 hover:bg-[#0070BA] transition-all duration-75 ${
+        currentAmount === coinsAmount
+          ? "bg-[#0070BA] outline outline outline-blue-400"
+          : "bg-[#467BA5]"
       }`}>
       {coinsAmount}
     </button>
